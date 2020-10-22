ed2da89275dcccf8e8718e75c26f6732b93ed7fe
Zhanyu Wu <wua@mail.smu.edu>
2020-10-22 01:34:59 -0500

    project2

--- FeatureTrackingApp.cpp
+++ FeatureTrackingApp.cpp
@@ -1,280 +1,221 @@
-/*
-
- Programmer: Courtney Brown
- Date: c2019
- Notes: Demonstration of sending OSC processing and creating artistic application using our previous code
- Purpose/Description:
-
- This program sends values from the mouse to another program.
-
- Uses:
-
- osc::Sender -- sends OSC messages from one program to another (incl. those running on another computer)
-
- Note: still does frame-differencing.
-
- TODO: (for you, student!)
- ++add your squares class
- ++find values to send from your squares class
-    ++either use frame-differencing or optical flow to send data (choose one)
-    ++must use a feature (eg. find max or min of each square + the location + send) to make an art or sound thing happen.
-    ++note that this example uses the mouse locations but you must sub. your mocap squares info.
-
- */
-
-
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/video/tracking.hpp>
-
+#include <opencv2/video.hpp>
  //includes for background subtraction
 #include "opencv2/imgproc.hpp"
 #include "opencv2/videoio.hpp"
 #include <opencv2/video.hpp>
+#include "opencv2/features2d.hpp"
 
-
+#include "CinderOpenCV.h"
 #include "cinder/app/App.h"
 #include "cinder/app/RendererGl.h"
 #include "cinder/gl/gl.h"
-#include "cinder/Capture.h" //needed for capture
-#include "cinder/Log.h" //needed to log errors
+#include "cinder/Capture.h" //add - needed for capture
+#include "cinder/Log.h" //add - needed to log errors
+#include "ExampleSquares.h"
+#define SAMPLE_WINDOW_MOD 300 //how often we find new features -- that is 1/300 frames we will find some features
+#define MAX_FEATURES 300 //The maximum number of features to track. Experiment with changing this number
 
-#include "Osc.h" //add to send OSC
 
-#include <math.h>
-
-#include "CinderOpenCV.h"
-
-using namespace ci;
+using namespace cinder;
 using namespace ci::app;
 using namespace std;
 
-//for networking
-#define LOCALPORT 8887 //we just bind here to send.
-#define DESTHOST "127.0.0.1" //this is sending to our OWN computer's IP address
-#define DESTPORT 8888 //this is the port we are sending to -- take note. This will have to match in the next code.
-
-//set up our OSC addresses
-#define WHERE_OSCADDRESS "/MakeItArt/Where"
-#define MOUSEDOWN_OSCADDRESS "/MakeItArt/Down"
-
-
-class MakeItArtMouseApp : public App {
-public:
-    void setup() override;
+class FeatureTrackingApp : public App {
+  public:
+	void setup() override;
+	void mouseDown( MouseEvent event ) override;
+	void update() override;
+	void draw() override;
     void keyDown(KeyEvent event) override;
-    void mouseDrag(MouseEvent event) override;
-    void mouseDown(MouseEvent event) override;
-    void mouseUp(MouseEvent event) override;
-
-
-
-    void update() override;
-    void draw() override;
-
-    MakeItArtMouseApp();
-
-    ci::vec2 curMousePosLastDown;
-    bool isMouseDown;
-
-
+   
+    
 protected:
-    CaptureRef                 mCapture;
-    gl::TextureRef             mTexture;
-
-    //for framedifferencing
-    cv::Mat                    mPrevFrame;
-    cv::Mat                    mFrameDifference;
-    ci::SurfaceRef             mSurface;
-
-    //Sending OSC
-    osc::SenderUdp                mSender; //sends the OSC via the UDP protocol
-    void sendOSC(std::string addr, float x, float y); //sending the OSC values
-    void sendOSC(std::string addr, float down);//just one -- this is inelegant but effective for now
-
+    CaptureRef                 mCapture; //uses video camera to capture frames of data.
+    gl::TextureRef             mTexture; //the current frame of visual data in OpenGL format.
+   
+    //for optical flow
+    vector<cv::Point2f>        mPrevFeatures, //the features that we found in the last frame
+                               mFeatures; //the feature that we found in the current frame
+    cv::Mat                    mPrevFrame; //the last frame
+    ci::SurfaceRef             mSurface; //the current frame of visual data in CInder format.
+    vector<uint8_t>            mFeatureStatuses; //a map of previous features to current features
+    cv::Ptr<cv::BackgroundSubtractor> mBackgroundSubtract;
+    void findOpticalFlow(); //finds the optical flow -- the visual or apparent motion of features (or persons or things or what you can detect/measure) through video
     void frameDifference(cv::Mat& outputImg);
-
+    bool mOpticalFlow = false;
+   
+    cv::Mat                    mFrameDifference;
+    SquaresFrameDiff           squareFrame; 
+    SquaresFeatures            squareFea; 
+    int N = 1;
 };
 
-MakeItArtMouseApp::MakeItArtMouseApp() : mSender(LOCALPORT, DESTHOST, DESTPORT) //initializing our class variables
+void FeatureTrackingApp::setup()
 {
-
-}
-
-//sets the mouse drag values
-void MakeItArtMouseApp::mouseDrag(MouseEvent event)
-{
-    curMousePosLastDown = event.getPos();
-    isMouseDown = true;
-
-}
-//sets the mouse down values
-void MakeItArtMouseApp::mouseDown(MouseEvent event)
-{
-    curMousePosLastDown = event.getPos();
-    isMouseDown = true;
-
-}
-
-
-//sets the mouse up values
-void MakeItArtMouseApp::mouseUp(MouseEvent event)
-{
-    isMouseDown = false;
-}
-
-
-void MakeItArtMouseApp::sendOSC(std::string addr, float x, float y) //sending the OSC values
-{
-    osc::Message msg;
-    msg.setAddress(addr); //sets the address
-
-    msg.append(x);
-    msg.append(y);
-
-    mSender.send(msg);
-}
-
-void MakeItArtMouseApp::sendOSC(std::string addr, float down) //sending the OSC values
-{
-    osc::Message msg;
-    msg.setAddress(addr); //sets the address
-
-    msg.append(down);
-
-    mSender.send(msg);
-}
-
-//initialization
-void MakeItArtMouseApp::setup()
-{
-    //set up our OSC sender and bind it to our local port
-    try {
-        mSender.bind();
-    }
-    catch (osc::Exception& e)
-    {
-        CI_LOG_E("Error binding" << e.what() << " val: " << e.value());
-        quit();
-    }
-
     //set up our camera
     try {
         mCapture = Capture::create(640, 480); //first default camera
         mCapture->start();
     }
-    catch (ci::Exception& exc)
+    catch( ci::Exception &exc)
     {
-        CI_LOG_EXCEPTION("Failed to init capture ", exc);
+        CI_LOG_EXCEPTION( "Failed to init capture ", exc ); //oh no!!
     }
+    
+    mPrevFrame.data = NULL; //initialize our previous frame to null since in the beginning... there no previous frames!
+    mBackgroundSubtract = cv::createBackgroundSubtractorKNN();
+}
 
-
-    mPrevFrame.data = NULL;
-    mFrameDifference.data = NULL;
+//maybe you will add mouse functionality!
+void FeatureTrackingApp::mouseDown( MouseEvent event )
+{
 
 }
 
-void MakeItArtMouseApp::keyDown(KeyEvent event)
+void FeatureTrackingApp::update()
 {
-    //TODO: save the current frame as the background image when user hits a key
-
-    //eg:
-    if (event.getChar() == ' ')
+    //update the current frame from camera
+    if(mCapture && mCapture->checkNewFrame()) //is there a new frame???? (& did camera get created?)
     {
-        //TODO: do a thing. Like save the current frame.
-    }
-
-}
-
-void MakeItArtMouseApp::update()
-{
-    //does frame-differencing stuff
-    if (mCapture && mCapture->checkNewFrame()) //is there a new frame???? (& did camera get created?)
-    {
-        mSurface = mCapture->getSurface();
-
-        if (!mTexture)
+        mSurface = mCapture->getSurface(); //get the current frame and put in the Cinder datatype.
+        
+        //translate the cinder frame (Surface) into the OpenGL one (Texture)
+        if(! mTexture)
             mTexture = gl::Texture::create(*mSurface);
         else
             mTexture->update(*mSurface);
-
-        //do the frame-differencing
-        frameDifference(mFrameDifference);
-
     }
-
-    //send the OSC re: mouse values
-    //& normalize the positions to 0. to 1. for easy scaling in processing program
-    sendOSC(WHERE_OSCADDRESS, (float)curMousePosLastDown.x / (float)getWindowWidth(), (float)curMousePosLastDown.y / (float)getWindowHeight());
-    sendOSC(MOUSEDOWN_OSCADDRESS, isMouseDown);
-
+    
+    if (!mOpticalFlow) {
+        findOpticalFlow();
+    }
+    else  frameDifference(mFrameDifference);
 }
 
-
-//find the difference between 2 frames + some useful image processing
-void MakeItArtMouseApp::frameDifference(cv::Mat& outputImg)
+void FeatureTrackingApp::findOpticalFlow()
 {
-
-    outputImg.data = NULL;
-    if (!mSurface) return;
-
-    //the current surface or frame in cv::Mat format
+    if(!mSurface) return; //don't go through with the rest if we can't get a camera frame!
+    
+    //convert gl::Texturer to the cv::Mat(rix) --> Channel() -- converts, makes sure it is 8-bit
     cv::Mat curFrame = toOcv(Channel(*mSurface));
+    
+    
+    //if we have a previous sample, then we can actually find the optical flow.
+    if( mPrevFrame.data ) {
+        
+        // pick new features once every SAMPLE_WINDOW_MOD frames, or the first frame
+        
+        //note: this means we are abandoning all our previous features every SAMPLE_WINDOW_MOD frames that we
+        //had updated and kept track of via our optical flow operations.
+        
+        if( mFeatures.empty() || getElapsedFrames() % SAMPLE_WINDOW_MOD == 0 ){
+            
+            /*
+             parameters for the  call to cv::goodFeaturesToTrack:
+             curFrame - img,
+             mFeatures - output of corners,
+             MAX_FEATURES - the max # of features,
+             0.005 - quality level (percentage of best found),
+             3.0 - min distance
+             
+             note: its terrible to use these hard-coded values for the last two parameters. perhaps you will fix your projects.
+             
+             note: remember we're finding corners/edges using these functions
+             */
+            cv::goodFeaturesToTrack( curFrame, mFeatures, MAX_FEATURES, 0.005, 3.0 );
+        }
+        
+        vector<float> errors; //there could be errors whilst calculating optical flow
+        
+        mPrevFeatures = mFeatures; //save our current features as previous one
+        
+        //This operation will now update our mFeatures & mPrevFeatures based on calculated optical flow patterns between frames UNTIL we choose all new features again in the above operation every SAMPLE_WINDOW_MOD frames. We choose all new features every couple frames, because we lose features as they move in and out frames and become occluded, etc.
+        if( ! mFeatures.empty() )
+            cv::calcOpticalFlowPyrLK( mPrevFrame, curFrame, mPrevFeatures, mFeatures, mFeatureStatuses, errors );
+        
+    }
+    
+    //set previous frame
+    mPrevFrame = curFrame;
+    
+}
+ 
+void FeatureTrackingApp::keyDown(KeyEvent event)
+{
+    
+    if (event.getChar() == '1') {
+     
 
-    if (mPrevFrame.data) {
-
-        //blur --> this means that it will be resilient to a little movement
-        //params are: cv::Mat Input1,
-//                    cv::Mat Result,
-//                    cv::Size - size of blur kernel (correlates to how blurred - must be positive & odd integers),
-//                               the bigger the size, the more the blur & also the larger the sigmas the more the blur.
-//                    double size of sigma X Gaussian kernel standard deviation in X direction
-//                    double size of sigma Y Gaussian kernel standard deviation in Y direction (optional, not used)
-//      More on Gaussian blurs here: https://homepages.inf.ed.ac.uk/rbf/HIPR2/gsmooth.htm
-//      Interestingly, we can think of them as a low-pass filter in 2D -- (if you know them from audio dsp, sound)
-        cv::GaussianBlur(curFrame, curFrame, cv::Size(3, 3), 0);
-
-        //find the difference
-        //params are: cv::Mat Input1, cv::Mat Input2, cv::Mat Result
-        cv::absdiff(curFrame, mPrevFrame, outputImg);
-
-        //take threshhold values -- think of this as image segmentation, see notes above in desc. header
-        //we will go further into image segmentation next week
-//        https://docs.opencv.org/2.4/modules/imgproc/doc/miscellaneous_transformations.html?highlight=threshold#threshold
-//    Parameters:
-//        src – input array (single-channel, 8-bit or 32-bit floating point).
-//        dst – output array of the same size and type as src.
-//        thresh – threshold value.
-//        maxval – maximum value to use with the THRESH_BINARY and THRESH_BINARY_INV thresholding types.
-//        type – thresholding type (see the details below).
-        cv::threshold(outputImg, outputImg, 50, 255, cv::THRESH_BINARY);
+        squareFrame.setN(10);
+        squareFea.setN(10);
+    }
+    if (event.getChar() == '2')
+    {
+        N = 20;
+        squareFrame.setN(30);
+        squareFea.setN(30);
+    }
+    if (event.getChar() == '3')
+    {
+        N = 40;
+        squareFrame.setN(60);
+        squareFea.setN(60);
+    }
+    if (event.getChar() == 'f') {
+        mOpticalFlow = !mOpticalFlow;
 
     }
-
-    mPrevFrame = curFrame;
 }
-
-void MakeItArtMouseApp::draw()
+void FeatureTrackingApp::draw()
 {
     gl::clear(Color(0, 0, 0));
 
-    gl::color(1, 1, 1, 1);
-    //
-    //    if( mTexture )
-    //    {
-    //        gl::draw( mTexture );
-    //    }
+    //color the camera frame normally
+    gl::color(1, 1, 1, 0.55);
 
-        //if the frame difference isn't null, draw it.
-    if (mFrameDifference.data)
+
+    //draw the camera frame
+    if (mTexture)
     {
-        gl::draw(gl::Texture::create(fromOcv(mFrameDifference)));
+        gl::draw(mTexture);
     }
+
+    // draw all the old points @ 0.5 alpha (transparency) as a circle outline
+    gl::color(1, 0, 0, 0.55);
+    for (int i = 0; i < mPrevFeatures.size(); i++)
+        gl::drawStrokedCircle(fromOcv(mPrevFeatures[i]), 3);
+
+
+    // draw all the new points @ 0.5 alpha (transparency)
+    gl::color(0, 0, 1, 0.5f);
+    for (int i = 0; i < mFeatures.size(); i++)
+        gl::drawSolidCircle(fromOcv(mFeatures[i]), 3);
+
+    //draw lines from the previous features to the new features
+    //you will only see these lines if the current features are relatively far from the previous
+    gl::color(0, 1, 0, 0.5f);
+    gl::begin(GL_LINES);
+    for (size_t idx = 0; idx < mFeatures.size(); ++idx) {
+        if (mFeatureStatuses[idx]) {
+            gl::vertex(fromOcv(mFeatures[idx]));
+            gl::vertex(fromOcv(mPrevFeatures[idx]));
+        }
+    }
+    gl::end();
+    if (mOpticalFlow) {
+        squareFea.drawRect(mFeatures);
+
+    }
+    else if
+         (!mOpticalFlow) {
+            gl::draw(gl::Texture::create(fromOcv(mFrameDifference)));
+            squareFrame.drawRect(mFrameDifference);
+        }
+
 
 }
 
-CINDER_APP(MakeItArtMouseApp, RendererGl,
-    [](MakeItArtMouseApp::Settings* settings) //note: this part is to fix the display after updating OS X 1/15/18
-    {
-        settings->setHighDensityDisplayEnabled(true);
-    })
+CINDER_APP( FeatureTrackingApp, RendererGl )
